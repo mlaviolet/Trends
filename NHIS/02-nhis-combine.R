c7ea31fc8c9000904da4fcacaba895af268d1984
Michael Laviolette <michael.j.laviolette@dhhs.nh.gov>
2021-07-20 09:54:14 -0400

    Matched results from SAS

--- NHIS/02-nhis-combine.R
+++ NHIS/02-nhis-combine.R
@@ -4,8 +4,7 @@
 library(here)
 library(tidyverse)
 library(srvyr)
-
-# dir(here("NHIS", "data"))
+options(survey.lonely.psu = "adjust")
 
 # for 2004 only, psu, stratum, age variables are in the person file instead of
 #   the sample adult file--process 2004 separately
@@ -18,6 +17,7 @@ samadult_04 <- readRDS(here("NHIS", "data", "2004", "samadult.rds")) %>%
 nhis_04 <- inner_join(person_04, samadult_04, 
                       by = c("srvy_yr", "hhx", "fmx", "fpx"))
 rm(person_04, samadult_04)
+
 # this function reads the person and sample adult files for each of the other
 #   years and joins them into an annual data table 
 readNHIS <- function(yr) {
@@ -33,6 +33,7 @@ readNHIS <- function(yr) {
   }
 
 # combine 16 yearly tables into a single table and make into survey object
+# combine all years except 2004
 nhis_dat <- map_dfr(sprintf("%02d", (0:15)[-5]), readNHIS) %>% 
   unnest(cols = srvy_yr) %>% 
   # add 2004 to table and arrange in year order
@@ -44,11 +45,6 @@ nhis_dat <- map_dfr(sprintf("%02d", (0:15)[-5]), readNHIS) %>%
   mutate(psu = if_else(srvy_yr %in% 2000:2005, psu, psu_p),
          stratum = if_else(srvy_yr %in% 2000:2005, stratum, strat_p)) %>% 
   select(-psu_p, -strat_p) %>% 
-  # same for otherpub and chip variables
-  # mutate(otherpub = if_else(srvy_yr %in% 2000:2007, otherpub, othpub),
-  #        chip = if_else(srvy_yr %in% 2000:2003, chip, schip)) %>% 
-  # select(-c(othpub, schip)) %>% 
-  # select(-c(hhx, fmx, px, fpx)) %>% 
   mutate(
     # group ages by 18-64 and 65+
     agegrp = cut(age_p, c(18, 65, Inf), c("18-64", "65+"),  right = FALSE), 
@@ -59,8 +55,7 @@ nhis_dat <- map_dfr(sprintf("%02d", (0:15)[-5]), readNHIS) %>%
 nhis_dat <- nhis_dat %>% 
   mutate(otherpub = if_else(srvy_yr > 2007, othpub, otherpub),
          chip = if_else(srvy_yr > 2003, schip, chip)) %>% 
-  # "OR UNKNOWN ON SUCH COVERAGE"--SHOULD UNKNOWN BE TREATED AS "NO"?
-  # IF SO, CHANGE NA_real_ to 2 below
+  # Medicaid or any public coverage?
   mutate(mcaid = case_when(
     srvy_yr %in% 2000:2003 & (medicaid %in% 1:2 | otherpub == 1 | chip == 1) ~ 1,
     srvy_yr %in% 2000:2003 & (medicaid == 3 | otherpub == 2 | chip == 2) ~ 2,
@@ -69,74 +64,53 @@ nhis_dat <- nhis_dat %>%
     srvy_yr %in% 2004:2015 & (medicaid == 3 | otherpub == 3 | chip == 3) ~ 2,
     srvy_yr %in% 2004:2015 & (medicaid %in% 7:9 | otherpub %in% 7:9 | chip %in% 7:9) ~ 9,
     TRUE ~ 9)) %>% 
+  # dichotomized private coverage
   mutate(pricov = case_when(private %in% 1:2 ~ 1,
-                            private == 3 ~ 2,
-                            private > 3 ~ 3,
-                            TRUE ~ NA_real_)) %>% 
+                            private == 3     ~ 2,
+                            private > 3      ~ 3,
+                            TRUE             ~ NA_real_)) %>% 
+  # hierarchy of uninsured, Medicaid, private, other
   mutate(instype = case_when(
-    notcov == 1 & pricov == 2 & mcaid == 2 ~ 1,
-    notcov == 2 & pricov == 2 & mcaid == 1 ~ 2,
-    notcov == 2 & pricov == 1              ~ 3,
-    notcov == 2 & pricov == 2 & mcaid == 2 ~ 4,
-    TRUE ~ NA_real_),
+    notcov == 1 & pricov == 2 & mcaid == 2 ~ 1, # uninsured
+    notcov == 2 & pricov == 2 & mcaid == 1 ~ 2, # Medicaid or other public
+    notcov == 2 & pricov == 1              ~ 3, # private
+    notcov == 2 & pricov == 2 & mcaid == 2 ~ 4, # other
+    TRUE                                   ~ NA_real_),
     instype = factor(instype, 1:4, 
                      c("Uninsured", "Medicaid", "Private", "Other"))) 
   
-
 # check unweighted counts for age 18-64
-n_18_64 <- nhis_dat %>% 
-  filter(agegrp == "18-64") %>% 
-  count(srvy_yr, instype)
+# n_18_64 <- nhis_dat %>% 
+#   filter(agegrp == "18-64") %>% 
+#   count(srvy_yr, instype)
+# looks good!
 
-  
-    # srvy_yr %in% 2004:2015 & (medicaid %in% 1:2 | otherpub %in% 1:2 | chip %in% 1:2) ~ 1,
-    # srvy_yr %in% 2004:2015 & (medicaid %in% 7:9 | otherpub %in% 7:9 | chip %in% 7:9) ~ 9,
-    # TRUE ~ 2)) 
-  # mutate(mcaid = case_when(medicaid %in% 1:2 ~ 1,
-  #                          medicaid == 3     ~ 2,
-  #                          TRUE              ~ NA_real_)) %>% 
-  # mutate(chp = case_when(srvy_yr %in% 2000:2003 & chip == 1     ~ 1,
-  #                        srvy_yr %in% 2000:2003 & chip == 2     ~ 2,
-  #                        srvy_yr %in% 2004:2015 & chip %in% 1:2 ~ 1,
-  #                        srvy_yr %in% 2004:2015 & chip == 3     ~ 2,
-  #                        TRUE                                   ~ NA_real_)) %>% 
-  # mutate(othpub = case_when(
-  #   srvy_yr %in% 2000:2003 & otherpub == 1     ~ 1,
-  #   srvy_yr %in% 2000:2003 & otherpub == 2     ~ 2,
-  #   srvy_yr %in% 2004:2015 & otherpub %in% 1:2 ~ 1,
-  #   srvy_yr %in% 2004:2015 & otherpub == 3     ~ 2,
-  #   TRUE                                       ~ 9)) %>% 
-  # mutate(mcaid = case_when(
-  #   medicaid %in% c(1:2, 7:9) | chp == 1 | othpub == 1 ~ 1,
-  #   medicaid == 3 & chp == 2 & othpub == 2     ~ 2,
-  #   TRUE                                       ~ 9)) %>% 
-  # TRANSLATE SAS CODE FOLLOWING "DICHOTOMIZED PRIVATE COVERAGE"
-  # create survey object for analysis
-
-nhis_svy <- as_survey_design(nhis_dat, ids = psu, strata = c(srvy_yr, stratum),
+# survey object  
+nhis_svy <- as_survey_design(nhis_dat, ids = psu, strata = c(stratum, srvy_yr),
                              weight = wtfa_sa, nest = TRUE)
-nhis_dat %>% 
-  mutate(across(c(agegrp, anyeruse), as.numeric)) %>% 
-  select(srvy_yr, age_p, otherpub, othpub, chip, schip, medicaid, private, 
-         notcov, anyeruse, psu, stratum, wtfa_sa) %>% 
-  write.csv(here("NHIS", "data", "nhis.csv"), row.names = FALSE, na = ".")
 
-# CHECK THAT PSU, STRATA, AND WEIGHTS ARE CORRECT
+# export data for SAS
+# nhis_dat %>% 
+#   mutate(across(c(agegrp, anyeruse), as.numeric)) %>% 
+#   select(srvy_yr, age_p, otherpub, othpub, chip, schip, medicaid, private, 
+#          notcov, anyeruse, psu, stratum, wtfa_sa) %>% 
+#   write.csv(here("NHIS", "data", "nhis.csv"), row.names = FALSE, na = ".")
+
+# CHECK THAT PSU, STRATA, AND WEIGHTS ARE CORRECT--AGAINST SUDAAN?
 rm(nhis_04)
     
-# chk4 <- count(nhis_svy$variables, srvy_yr, medicaid)
-# # ADD ANALYSIS
-# 
-# er_use <- nhis_svy %>% 
-#   filter(mcaid == 1) %>% 
-#   # filter(!is.na(anyeruse)) %>% 
-#   # filter(agegrp == "18-64") %>% 
-#   group_by(agegrp, srvy_yr) %>% 
-#   summarize(pct = survey_mean(anyeruse == "One or more", na.rm = TRUE)) %>% 
-#   mutate(across(starts_with("pct"), ~ 100 * .x))
-# # seems consistently lower--should I count unknowns with Yes, as implied by
-# #   notes in SAS code?
-
+# READY FOR ANALYSIS
+results <- nhis_svy %>% 
+  filter(agegrp == "18-64") %>% 
+  #, !is.na(anyeruse), !is.na(INSTYPE)
+  group_by(srvy_yr, instype) %>% 
+  summarize(n = unweighted(n()),
+            pct = survey_mean(anyeruse == "One or more", na.rm = TRUE)) %>% 
+  filter(instype != "Other") %>% 
+  # arrange(srvy_yr, instype) %>% 
+  mutate(across(starts_with("pct"), ~ 100 * .x))
+# point estimates match
+# std errors slightly off but match SAS--why? would SUDAAN match?
 
 
 
--- NHIS/test.sas
+++ NHIS/test.sas
@@ -1,4 +1,10 @@
 *--------1---------2---------3---------4---------5---------6---------7---------8;
+/* REPRODUCE RESULTS IN EXAMPLE A, 
+   National Center for Health Statistics, Guidelines for Analysis of Trends
+   https://www.cdc.gov/nchs/data/series/sr_02/sr02_179.pdf */
+
+DM "CLEAR LOG; CLEAR OUT;" ;
+
 proc format;
    value anyeruse
    1 = "None"
@@ -12,6 +18,8 @@ proc format;
    ;
 run;
 
+/* multiyear data table from CSV exported from R
+   crude frequencies match */
 PROC IMPORT OUT= WORK.nhis 
             DATAFILE= "C:\Users\michael.j.laviolette\Desktop\Trends\NHIS\data\nhis.csv" 
             DBMS=CSV REPLACE;
@@ -19,74 +27,84 @@ PROC IMPORT OUT= WORK.nhis
      DATAROW=2; 
 RUN;
 
-data nhis_test;
+data nhis00_15;
   set nhis;
+  /* CODE PROVIDED BY NCHS */
 ************* HEALTH INSURANCE****************;
-IF SRVY_YR > 2007 
-THEN DO;
-       OTHERPUB = OTHPUB;
-     END;
-IF SRVY_YR > 2003 
-THEN DO
-       CHIP = SCHIP;
-     END;
-IF SRVY_YR < 2004 
-THEN DO;
-       *** MEDICAID/PUBLIC ASSISTANCE ;
-       *** MCAID=1 IF YES, HAS MEDICAID OR PUBLIC ASSISTANCE ;
-       *** MCAID=0 IF NO, DOES NOT HAVE MEDICAID OR PUBLIC ASSISTANCE OR 
-           UNKNOWN ON SUCH COVERAGE ;
-       IF MEDICAID IN (1,2) OR OTHERPUB IN (1) OR CHIP IN (1)
-       THEN MCAID = 1;
-       ELSE IF (MEDICAID > 3 OR MEDICAID <= 0) OR (CHIP > 2 OR CHIP <= 0) OR 
-               (OTHERPUB > 2 OR OTHERPUB <= 0) 
-            THEN MCAID = 9;
-            ELSE MCAID = 2;
-     END;
-ELSE IF SRVY_YR > 2003 
-     THEN DO;
-            *** MEDICAID/PUBLIC ASSISTANCE;
-            *** MCAID = 1 IF YES, HAS MEDICAID OR PUBLIC ASSISTANCE ;
-            *** MCAID = 0 IF NO, DOES NOT HAVE MEDICAID OR PUBLIC ASSISTANCE
-                OR UNKNOWN ON SUCH COVERAGE ;
-            IF MEDICAID IN (1,2) OR OTHERPUB IN (1,2) OR CHIP IN (1,2)
-            THEN MCAID = 1;
-            ELSE IF (MEDICAID > 3 OR MEDICAID <= 0) OR (CHIP > 3 OR CHIP <= 0) OR (OTHERPUB > 3 OR OTHERPUB <= 0) 
-                 THEN MCAID = 9;
-                 ELSE MCAID = 2;
-          END;
-*DICHOTOMIZED PRIVATE COVERAGE;
-IF PRIVATE IN (1 2) 
-THEN PRICOV = 1;
-ELSE IF PRIVATE = 3 
-     THEN PRICOV = 2;
-     ELSE IF PRIVATE > 3 
-          THEN PRICOV = 3;
-*THIS CODE IS A HIERARCHY, COULD ALSO USE THE OVERLAPPING CODE FROM HUS;
-IF NOTCOV = 1 AND PRICOV = 2 AND MCAID = 2 
-THEN INSTYPE = 1; *UNINSURED, NOT PRIVATE, NOT MEDICAID;
-IF NOTCOV = 2 AND PRICOV = 2 AND MCAID = 1 
-THEN INSTYPE = 2; *MEDICAID (INCL OTHPUB AND CHIP), NOT PRIVATE, NOT UNINSURED;
-IF NOTCOV = 2 AND PRICOV = 1 
-THEN INSTYPE = 3; *PRIVATE, NOT MEDICAID, NOT UNINSURED;
-IF NOTCOV = 2 AND PRICOV = 2 AND MCAID = 2 
-THEN INSTYPE = 4; *OTHER INSURED -- ABOUT 10%;
-
-under65 = (18 <= age_p <=64);
-format instype instype. anyeruse anyeruse.;
+  IF SRVY_YR > 2007 
+  THEN DO;
+         OTHERPUB = OTHPUB;
+       END;
+  IF SRVY_YR > 2003 
+  THEN DO
+         CHIP = SCHIP;
+       END;
+  IF SRVY_YR < 2004 
+  THEN DO;
+         *** MEDICAID/PUBLIC ASSISTANCE ;
+         *** MCAID=1 IF YES, HAS MEDICAID OR PUBLIC ASSISTANCE ;
+         *** MCAID=0 IF NO, DOES NOT HAVE MEDICAID OR PUBLIC ASSISTANCE OR 
+             UNKNOWN ON SUCH COVERAGE ;
+         IF MEDICAID IN (1,2) OR OTHERPUB IN (1) OR CHIP IN (1)
+         THEN MCAID = 1;
+         ELSE IF (MEDICAID > 3 OR MEDICAID <= 0) OR (CHIP > 2 OR CHIP <= 0) OR 
+                 (OTHERPUB > 2 OR OTHERPUB <= 0) 
+              THEN MCAID = 9;
+              ELSE MCAID = 2;
+       END;
+  ELSE IF SRVY_YR > 2003 
+       THEN DO;
+              *** MEDICAID/PUBLIC ASSISTANCE;
+              *** MCAID = 1 IF YES, HAS MEDICAID OR PUBLIC ASSISTANCE ;
+              *** MCAID = 0 IF NO, DOES NOT HAVE MEDICAID OR PUBLIC ASSISTANCE
+                  OR UNKNOWN ON SUCH COVERAGE ;
+              IF MEDICAID IN (1,2) OR OTHERPUB IN (1,2) OR CHIP IN (1,2)
+              THEN MCAID = 1;
+              ELSE IF (MEDICAID > 3 OR MEDICAID <= 0) OR (CHIP > 3 OR CHIP <= 0) OR (OTHERPUB > 3 OR OTHERPUB <= 0) 
+                   THEN MCAID = 9;
+                   ELSE MCAID = 2;
+            END;
+  *DICHOTOMIZED PRIVATE COVERAGE;
+  IF PRIVATE IN (1 2) 
+  THEN PRICOV = 1;
+  ELSE IF PRIVATE = 3 
+       THEN PRICOV = 2;
+       ELSE IF PRIVATE > 3 
+            THEN PRICOV = 3;
+  *THIS CODE IS A HIERARCHY, COULD ALSO USE THE OVERLAPPING CODE FROM HUS;
+  IF NOTCOV = 1 AND PRICOV = 2 AND MCAID = 2 
+  THEN INSTYPE = 1; *UNINSURED, NOT PRIVATE, NOT MEDICAID;
+  IF NOTCOV = 2 AND PRICOV = 2 AND MCAID = 1 
+  THEN INSTYPE = 2; *MEDICAID (INCL OTHPUB AND CHIP), NOT PRIVATE, NOT UNINSURED;
+  IF NOTCOV = 2 AND PRICOV = 1 
+  THEN INSTYPE = 3; *PRIVATE, NOT MEDICAID, NOT UNINSURED;
+  IF NOTCOV = 2 AND PRICOV = 2 AND MCAID = 2 
+  THEN INSTYPE = 4; *OTHER INSURED -- ABOUT 10%;
+  /* add variable for age 18-64, subject of analysis */
+  under65 = (18 <= age_p <= 64);
+  format instype instype. anyeruse anyeruse.;
+  if instype NE 4;
 run;
 
 title "Ages 18-64";
-proc freq data = nhis_test;
+proc freq data = nhis00_15;
   where(18 <= age_p <= 64);
-  table srvy_yr * instype /nocum nopercent norow nocol missing;
+  table srvy_yr * (mcaid pricov instype) /nocum nopercent norow nocol missing;
+run;
+
+proc sort data =nh00_15;
+  by stratum psu;
 run;
 
 
-proc surveyfreq data = nhis_test nomcar;
+proc surveyfreq data = nhis00_15 nomcar;
+  *ods output ResponseTable = Results;
   strata stratum;
   cluster psu;
   weight wtfa_sa;
   table under65 * srvy_yr * instype * anyeruse / row nototal;
 run;
+/* POINT ESTIMATES MATCH, STD ERRORS SLIGHTLY OFF FROM PUBLISHED
+   WOULD THEY MATCH IN SUDAAN? */
+title;
 
